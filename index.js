5bcec6d7256fdd427bb425aa4c3ad9dcffb43fda
Robyn Sanders <reddie@smartchat.net.au>
2021-09-13 12:20:20 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    function clickAlert() {
+        alert('I was clicked!');
+      }
+      
+      input.addEventListener('click', clickAlert);
 }
+addingEventListener();
+
+    function clickAlert() {
+        alert('I was clicked!');
+      }
+      
+      input.addEventListener('click', clickAlert);
+
